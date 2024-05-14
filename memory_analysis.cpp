a18208f26554805e00fd3e8803e1e4ffc33472d8
Ibragim04 <149951420+Ibragim04@users.noreply.github.com>
2024-05-14 22:11:11 +0300

    Create memory_analysis.cpp

---
+++ memory_analysis.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main() {
+    std::cout << "Size of std::vector: " << sizeof(std::vector<int>) << " bytes" << std::endl;
+    std::cout << "Size of std::list: " << sizeof(std::list<int>) << " bytes" << std::endl;
+    return 0;
+}
